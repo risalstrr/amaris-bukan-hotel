5744187a0d2e85f83c7ba0b5f589d31a47dd7170
risalstr <risa.lestari2002@gmail.com>
2024-07-13 00:44:33 +0700

    feat:add domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['pelitariau.com', 'media.istockphoto.com', 'cdn.prod.website-files.com', 'img.freepik.com', '5.imimg.com'],
+        domains: ['pelitariau.com', 'media.istockphoto.com', 'cdn.prod.website-files.com', 'img.freepik.com', '5.imimg.com', 'i.ytimg.com', 'encrypted-tbn0.gstatic.com'],
     },
 };
 
